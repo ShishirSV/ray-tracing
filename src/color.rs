495301e7d9ec3b84c96adfe57087c685d147c30d
ShishirSV <volety.s@northeastern.edu>
2026-03-28 15:19:08 +0530

    completed color.rs

--- src/color.rs
+++ src/color.rs
@@ -4,3 +4,17 @@ use crate::vec3::Vec3;
 pub struct Color {
     pub rgb: Vec3,
 }
+
+impl Color {
+    pub fn new(red: f64, green: f64, blue: f64) -> Self {
+        Self {
+            rgb: Vec3::new(red, green, blue),
+        }
+    }
+}
+
+impl From<Vec3> for Color {
+    fn from(vector: Vec3) -> Self {
+        Self { rgb: vector }
+    }
+}
